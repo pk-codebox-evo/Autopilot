f1145a81b753fdc209dc3009fc886d4f1d0ef419
Artur Heinze <artur@yootheme.com>
2015-02-13 15:58:57 +0100

    cleanup

--- bootstrap.php
+++ bootstrap.php
@@ -45,7 +45,7 @@ $this->module("autopilot")->extend([
             "class"    => ""
         ], $options);
 
-        $items = $this->app->helper("autopilot.menu")->getMenu($name);
+        $items = $this->helper("menu")->getMenu($name);
         $level = 0;
 
         if (!count($items)) {
@@ -70,24 +70,19 @@ $this->module("autopilot")->extend([
     },
 
     "getMenues" => function() {
-
-        return $this->app->helper('autopilot.menu')->getMenues();
+        return $this->helper('menu')->getMenues();
     },
 
     "getMenu" => function($menu) {
-
-        return $this->app->helper('autopilot.menu')->getMenu($menu);
-
+        return $this->helper('menu')->getMenu($menu);
     },
 
     "getFlattenMenues" => function() {
-
-        return $this->app->helper('autopilot.menu')->flatten();
+        return $this->helper('menu')->flatten();
     },
 
     "getFlattenMenu" => function($name) {
-
-        return $this->app->helper('autopilot.menu')->flatten($name);
+        return $this->helper('menu')->flatten($name);
     },
 
     "settings" => function() {
@@ -99,7 +94,6 @@ $this->module("autopilot")->extend([
     },
 
     "getPositions" => function() {
-
         return isset($this->thememeta['positions']) ? $this->thememeta['positions']:['Sidebar'];
     },
 
@@ -189,217 +183,9 @@ $app->helpers["autopilot.shortcodes"] = 'Autopilot\\Helper\\Shortcodes';
 $app->helpers["autopilot.widgets"]    = 'Autopilot\\Helper\\Widgets';
 $app->helpers["autopilot.filters"]    = 'Autopilot\\Helper\\Filters';
 
-// register core types
-
-$module->registerType('link', [
-    'name'   => 'Link',
-    'views'  => [ 'edit' => 'autopilot:types/link/admin/edit.php']
-]);
-
-$module->registerType('page', [
-    'name'   => 'Page',
-    'views'  => [ 'edit' => 'autopilot:types/page/admin/edit.php'],
-    'render' => function($link, $data) use($module) {
-
-        // look in the theme first
-        $view = $module->frontend->path("theme:types/page/render.php");
-
-        if (!$view) {
-            $view = $module->app->path("custom:autopilot/types/page/render.php");
-        }
-
-        // fall back to default
-        if (!$view) {
-            $view = $module->app->path("autopilot:types/page/render.php");
-        }
-
-        if ($view) {
-            return $module->frontend->view($view.' with theme:layout.php', compact('data', 'link'));
-        }
-
-        return false;
-    }
-]);
-
-$module->registerType('php-file', [
-    'name'   => 'PHP Script',
-    'views'  => [ 'edit' => 'autopilot:types/php-file/admin/edit.php'],
-    'render' => function($link, $data) use($module) {
-
-        // look in the theme first
-        $view = $module->frontend->path("theme:types/php-file/render.php");
-
-        if (!$view) {
-            $view = $module->app->path("custom:autopilot/types/php-file/render.php");
-        }
-
-        // fall back to default
-        if (!$view) {
-            $view = $module->app->path("autopilot:types/php-file/render.php");
-        }
-
-        if ($view) {
-            return $module->frontend->view($view.' with theme:layout.php', compact('data', 'link'));
-        }
-
-        return false;
-    }
-]);
-
-$module->registerType('collection', [
-    'name'   => 'Collection',
-    'views'  => [ 'edit' => 'autopilot:types/collection/admin/edit.php'],
-    'route'  => function($link) use($module) {
-
-        $data        = $module->helper("menu")->getItemData($link['_id']);
-        $listroute   = $link['slug_path'];
-        $detailroute = $link['slug_path'].'/*';
-
-        if ($link['home']) {
-           $listroute = '/';
-        }
-
-        // bind list view
-        $module->frontend->bind($listroute, function() use($link, $data, $module) {
-
-            if (!isset($data['collectionId']) && !$data['collectionId']) {
-                return false;
-            }
-
-            $collection = $module->app->db->findOne('common/collections', ['_id'=>$data['collectionId']]);;
-
-            if (!$collection) {
-                return false;
-            }
-
-            $this->meta['title']    = $link['title'];
-            $this->meta['menuItem'] = $link['_id'];
-
-            // set page keywords
-            if (isset($data['meta']['keywords']) && $data['meta']['keywords']) {
-                $this->meta['keywords'] = @$data['meta']['keywords'];
-            }
-
-            // set page description
-            if (isset($data['meta']['description']) && $data['meta']['description']) {
-                $this->meta['description'] = @$data['meta']['description'];
-            }
-
-            $colname = $collection['name'];
-
-            // look in the theme first
-            $view = $this->path("theme:types/collection/{$colname}/list.php");
-
-            if (!$view) {
-                $view = $module->app->path("custom:autopilot/types/collection/{$colname}/list.php");
-            }
-
-            if (!$view) {
-                $view = $module->app->path("custom:autopilot/types/collection/list.php");
-            }
-
-            // fall back to default
-            if (!$view) {
-                $view = $module->app->path("autopilot:types/collection/list.php");
-            }
-
-            if ($view) {
-
-                $limit = 10;
-                $page  = $this->param("page", 1);
-                $count = $module->app->module('collections')->collection($colname)->count();
-                $pages = ceil($count/$limit);
-
-                $items = $module->app->module('collections')->find($colname, [
-                    'limit' => $limit,
-                    'skip'  => ($page-1) * $limit
-                ]);
-
-                return $module->frontend->view($view.' with theme:layout.php', compact('link', 'data', 'items', 'page', 'pages'));
-            }
-
-            return false;
-        });
-
-        // bind detail view
-        $module->frontend->bind($detailroute, function($params) use($link, $data, $module) {
-
-            if (!isset($data['collectionId']) && !$data['collectionId']) {
-                return false;
-            }
-
-            $collection = $module->app->db->findOne('common/collections', ['_id'=>$data['collectionId']]);;
-
-            if (!$collection) {
-                return false;
-            }
-
-            $colname = $collection['name'];
-
-            // look in the theme first
-            $view = $this->path("theme:types/collection/{$colname}/item.php");
-
-            if (!$view) {
-                $view = $module->app->path("custom:autopilot/types/collection/{$colname}/item.php");
-            }
-
-            if (!$view) {
-                $view = $module->app->path("custom:autopilot/types/collection/item.php");
-            }
-
-            // fall back to default
-            if (!$view) {
-                $view = $module->app->path("autopilot:types/collection/item.php");
-            }
-
-            if ($view) {
-
-                $routeparts = explode('-', $this['route']);
-                $item       = $module->app->module('collections')->findOne($colname, ['_id' => array_pop($routeparts)]);
-
-                if (!$item) {
-                    return false;
-                }
-
-                return $module->frontend->view($view.' with theme:layout.php', compact('widget', 'settings', 'item'));
-            }
-
-            return false;
-
-
-            return 'detail';
-        });
-    }
-]);
-
-// register core widgets
-
-$module->registerWidget('content', [
-    'name'   => 'Content',
-    'views'  => [ 'edit' => 'autopilot:widgets/content/admin/edit.php'],
-    'render' => function($widget) use($module) {
-
-        $settings = &$widget['settings'];
-
-        // look in the theme first
-        $view = $module->frontend->path("theme:widgets/content/widget.php");
-
-        if (!$view) {
-            $view = $module->app->path("custom:autopilot/widgets/content/widget.php");
-        }
-
-        // fall back to default
-        if (!$view) {
-            $view = $module->app->path("autopilot:widgets/content/widget.php");
-        }
-
-        if ($view) {
-            return $module->frontend->view($view, compact('widget', 'settings'));
-        }
-
-        return false;
-    }
-]);
+// register core types + widgets
+include_once(__DIR__.'/types/boot.php');
+include_once(__DIR__.'/widgets/boot.php');
 
 // set theme path
 if ($themepath = $app->path('custom:autopilot/theme')) {
@@ -421,18 +207,12 @@ $app->on('cockpit.bootstrap', function() use($module) {
 
     # try to fix relative urls
     $module->helper('filters')->add('content', function($content){
-
         return $this->helper('utils')->fixRelativeUrls($content, $this->app->baseUrl('site:'));
-
     }, -100);
 
     # apply short codes
     $module->helper('filters')->add('content', function($content){
-
-        $content = $this->app->helper('autopilot.shortcodes')->do_shortcode($content);
-
-        return $content;
-
+        return $this->app->helper('autopilot.shortcodes')->do_shortcode($content);
     }, 10);
 
     $this->trigger('autopilot.bootstrap');
